527fe5ce1d9e719428c5f10efad9683179ee86c3
Ashutosh2002 <ashutoshswarup2002@gmail.com>
2020-12-24 15:12:44 +0530

    First commit

---
+++ lib/widgets/adaptive_button.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveFlatButton extends StatelessWidget {
+  final String text;
+  final Function handler;
+
+  AdaptiveFlatButton(this.text, this.handler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: handler,
+          )
+        : FlatButton(
+            onPressed: handler,
+            textColor: Theme.of(context).primaryColor,
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
+  }
+}
